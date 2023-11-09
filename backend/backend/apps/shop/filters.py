6bb15dc310fa6dbf4a06dc20b5d086b27f07da74
zotthy <se05110800@gmail.com>
2023-11-09 20:02:19 +0100

    filter

---
+++ backend/backend/apps/shop/filters.py
@@ -0,0 +1,10 @@
+from rest_framework import generics
+from django_filters import rest_framework as filters
+from shop.models import Product
+
+class ProductFilter(filters.FilterSet):
+    name = filters.CharFilter(field_name = 'name', lookup_expr = 'icontains')
+    
+    class Meta:
+        model = Product
+        fields = ['name']
