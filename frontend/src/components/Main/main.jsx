eb9d28134ce0bbe0c32e5c69754ef9be44bffc8b
YoungGinger <105806245+YoungGinger@users.noreply.github.com>
2023-11-10 17:21:18 +0100

    main section, popular product, product

--- frontend/src/components/Main/main.jsx
+++ frontend/src/components/Main/main.jsx
@@ -1,40 +1,84 @@
-import React from 'react'
-import './main.css'
-import cup from '../../images/Cup.png'
+import React from 'react';
+import './main.css';
+import cup from '../../images/Cup.png';
+
 
 function Product({image, name}) {
   return (
     <div className='product'>
-             <img src={image} alt="" className="product-picture" />
+             <img src={cup} alt="" className="product-picture" />
             <div className='product-name'>
-              <p>{name}</p>
+              <p>lorem</p>
             </div>
           </div>
   );
 }
 
+function PopularProduct({image, name}) {
+  return (
+    <div className='popular-product'>
+             <img src={cup} alt="" className="popular-product-picture" />
+            <div className='popular-product-name'>
+              <p>Lorem</p>
+            </div>
+          </div>
+  );
+}
+
+
 const main = () => {
   return (
-    <div>
-      <div className='info-bar'>
-        <p>Popular categories</p>
-        <button>View all</button> 
+    <div className='main-container'>
+      <div className='left-section'>
+
       </div>
-      <div className='container'>
-        <div className='main'>
-          <Product />
-          <Product />
-          <Product />
-          <Product />
-          <Product />
-          <Product />
-          <Product />
-          <Product />
-          <Product />
-          <Product />
-          <Product />
-          <Product />
+      <div className="center-section">
+        <div>
+          <div className='info-bar'>
+              <p>Popular categories</p>
+            </div>
+            <div className='container'>
+                <div className='main'>
+                  <Product />
+                  <Product />
+                  <Product />
+                  <Product />
+                  <Product />
+                  <Product />
+                  <Product />
+                  <Product />
+                  <Product />
+                  <Product />
+                  <Product />
+                  <Product />
+                </div>
+            </div>
+          </div>
+
+          <div>
+          <div className='info-bar'>
+              <p>Popular Product</p>
+            </div>
+            <div className='container'>
+                <div className='popular-main'>
+                  <PopularProduct />
+                  <PopularProduct />
+                  <PopularProduct />
+                  <PopularProduct />
+                  <PopularProduct />
+                  <PopularProduct />
+                  <PopularProduct />
+                  <PopularProduct />
+                  <PopularProduct />
+                  <PopularProduct />
+                </div>
+            </div>
+          </div>
         </div>
+      
+       
+      <div className='right-section'>
+
       </div>
     </div>
   )
