16817aeca8465d94e631ab826132656e775379ba
zotthy <se05110800@gmail.com>
2023-11-11 10:26:20 +0100

    adding a product

--- backend/backend/apps/shop/urls.py
+++ backend/backend/apps/shop/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from shop import views
 
 urlpatterns = [
-    path('products/', views.ProductList.as_view(), name='product-list')
+    path('products/', views.ProductList.as_view(), name='product-list'),
+    path('products/add/',views.ProductCreate.as_view())
 ]
--- backend/backend/apps/shop/views.py
+++ backend/backend/apps/shop/views.py
@@ -1,4 +1,4 @@
-from rest_framework.generics import ListAPIView
+from rest_framework import generics
 from shop.serializer import ProductSerializer
 from shop.models import Product
 from rest_framework.pagination import PageNumberPagination
@@ -6,10 +6,14 @@ from django_filters.rest_framework import DjangoFilterBackend
 from shop.filters import ProductFilter
 
 
-class ProductList(ListAPIView):
+class ProductList(generics.ListAPIView):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
     permission_classes = ()
     pagination_class = PageNumberPagination
     filter_backends = (DjangoFilterBackend,)
     filterset_class = ProductFilter
+
+class ProductCreate(generics.CreateAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
