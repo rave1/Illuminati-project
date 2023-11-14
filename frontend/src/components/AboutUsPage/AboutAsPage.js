e8688d4d8b6e4916f45977bf5fba5d95a88af0e3
Jakub <jakubsopel7@gmail.com>
2023-11-14 20:42:38 +0100

    React Router implemented (#10)
    Co-authored-by: Patryk <patryk17x@gmail.com>

---
+++ frontend/src/components/AboutUsPage/AboutAsPage.js
@@ -0,0 +1,25 @@
+import React from "react";
+
+export const AboutUsPage = () => {
+    return (
+        <div>
+            <ul>
+                <li>AboutAs</li>
+                <li>AboutAs</li>
+                <li>AboutAs</li>
+                <li>AboutAs</li>
+                <li>AboutAs</li>
+                <li>AboutAs</li>
+                <li>AboutAs</li>
+                <li>AboutAs</li>
+                <li>AboutAs</li>
+                <li>AboutAs</li>
+                <li>AboutAs</li>
+                <li>AboutAs</li>
+                <li>AboutAs</li>
+                <li>AboutAs</li>
+            </ul>
+        </div>
+    )
+}
+
