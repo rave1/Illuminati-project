56681eeab7c8d65090c86688a100158dbc58e26d
zotthy <se05110800@gmail.com>
2023-11-14 20:21:17 +0100

    product add corected

--- backend/backend/apps/shop/urls.py
+++ backend/backend/apps/shop/urls.py
@@ -3,6 +3,5 @@ from django.urls import path
 from shop import views
 
 urlpatterns = [
-    path('products/', views.ProductList.as_view(), name='product-list'),
-    path('products/add/',views.ProductCreate.as_view())
-]
+    path('products/', views.ProductCreate.as_view(), name='product-list'),
+]   
--- backend/backend/apps/shop/views.py
+++ backend/backend/apps/shop/views.py
@@ -4,9 +4,11 @@ from shop.models import Product
 from rest_framework.pagination import PageNumberPagination
 from django_filters.rest_framework import DjangoFilterBackend
 from shop.filters import ProductFilter
+from rest_framework.generics import ListCreateAPIView
+from rest_framework.generics import ListAPIView
 
 
-class ProductList(generics.ListAPIView):
+class ProductList(ListAPIView):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
     permission_classes = ()
@@ -14,6 +16,6 @@ class ProductList(generics.ListAPIView):
     filter_backends = (DjangoFilterBackend,)
     filterset_class = ProductFilter
 
-class ProductCreate(generics.CreateAPIView):
+class ProductCreate(ListCreateAPIView):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
