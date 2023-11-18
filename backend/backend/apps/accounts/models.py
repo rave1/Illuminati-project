b0fb2cd63d74cbd0a4ff584687853f9e7e07cddf
zotthy <se05110800@gmail.com>
2023-11-18 11:51:21 +0100

    add field age and image

--- backend/backend/apps/accounts/models.py
+++ backend/backend/apps/accounts/models.py
@@ -3,4 +3,13 @@ from django.contrib.auth.models import AbstractUser
 
 
 class User(AbstractUser):
-    pass
+    image = models.ImageField(blank=True,null=True)
+    age = models.DateField(blank=True,null=True)
+
+    @property
+    def display_age(self):
+        return self.age
+
+    @property
+    def display_image(self):
+        return self.image
--- backend/backend/apps/shop/views.py
+++ backend/backend/apps/shop/views.py
@@ -1,12 +1,12 @@
+from rest_framework.generics import ListAPIView
 from shop.serializer import ProductSerializer
 from shop.models import Product
 from rest_framework.pagination import PageNumberPagination
 from django_filters.rest_framework import DjangoFilterBackend
 from shop.filters import ProductFilter
-from rest_framework.generics import ListAPIView,ListCreateAPIView
 
 
-class ProductList(ListCreateAPIView):
+class ProductList(ListAPIView):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
     permission_classes = ()
