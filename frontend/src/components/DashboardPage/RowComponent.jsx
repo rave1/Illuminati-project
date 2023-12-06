f90f14f71aeeb7c7d162a2a715de2b5c90d0472b
Jakub Sopel <jakubsopel7@gmail.com>
2023-12-06 19:50:31 +0100

    wip: order dashboard

---
+++ frontend/src/components/DashboardPage/RowComponent.jsx
@@ -0,0 +1,33 @@
+export const DashboardRow = (props) => {
+    return (
+        <tr>
+        <th>
+            <label>
+                <input type="checkbox" className="checkbox" />
+            </label>
+        </th>
+        <td>
+            <div className="flex items-center gap-3">
+                <div className="avatar">
+                    <div className="mask mask-squircle w-12 h-12">
+                        <img src={Cup} alt="Avatar Tailwind CSS Component" />
+                    </div>
+                </div>
+                <div>
+                    <div className="font-bold">Hart Hagerty</div>
+                    <div className="text-sm opacity-50">United States</div>
+                </div>
+            </div>
+        </td>
+        <td>
+            Zemlak, Daniel and Leannon
+            <br />
+            <span className="badge badge-ghost badge-sm">Desktop Support Technician</span>
+        </td>
+        <td>Purple</td>
+        <th>
+            <button className="btn btn-ghost btn-xs">details</button>
+        </th>
+    </tr>
+    )
+}
