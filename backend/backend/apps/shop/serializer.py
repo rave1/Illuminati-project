c279822f3b22c26c803b4bfb8cd1899ba5e8be1f
Sam Sepiol <jakubsopel7@gmail.com>
2023-12-06 21:50:08 +0100

    feat: create orders in backend

--- backend/backend/apps/shop/serializer.py
+++ backend/backend/apps/shop/serializer.py
@@ -1,21 +1,56 @@
 from rest_framework import serializers
 from shop.models import Product, Order
+from accounts.models import User
 
 
 class ProductSerializer(serializers.ModelSerializer):
-
     class Meta:
         model = Product
-        fields = (
-            'id', 'name', 'price', 'quantity',
-            'description', 'image'
-        )
+        fields = ("id", "name", "price", "quantity", "description", "image")
+
 
 class OrderSerializer(serializers.ModelSerializer):
+    products = ProductSerializer(many=True)
 
     class Meta:
         model = Order
         fields = (
-            'id', 'city', 'zip_code', 'street', 'house_number',
-            'status', 'products', 'user'
+            "id",
+            "city",
+            "zip_code",
+            "street",
+            "house_number",
+            "status",
+            "products",
+            "user",
+        )
+
+
+class OrderCreateSerializer(serializers.ModelSerializer):
+    user = serializers.IntegerField(required=False, write_only=True)
+    products = serializers.IntegerField(required=False, write_only=True)
+    products_ids = serializers.ListField(write_only=True)
+
+    def create(self, validated_data):
+        validated_data["user"] = self.context["request"].user
+        products_ids = validated_data.pop("products_ids")
+        order = super().create(validated_data)
+        print(validated_data, products_ids)
+        for product in products_ids:
+            for product_id, quant in product.items():
+                order.products.add(Product.objects.get(id=product["id"]))
+        return order
+
+    class Meta:
+        model = Order
+        fields = (
+            "id",
+            "user",
+            "products",
+            "city",
+            "zip_code",
+            "street",
+            "house_number",
+            "price",
+            "products_ids",
         )
--- backend/backend/apps/shop/views.py
+++ backend/backend/apps/shop/views.py
@@ -1,5 +1,5 @@
 from rest_framework.generics import ListAPIView, ListCreateAPIView
-from shop.serializer import ProductSerializer, OrderSerializer
+from shop.serializer import ProductSerializer, OrderSerializer, OrderCreateSerializer
 from shop.models import Product, Order
 from rest_framework.pagination import PageNumberPagination
 from django_filters.rest_framework import DjangoFilterBackend
@@ -26,3 +26,10 @@ class OrderList(ListCreateAPIView):
 
     def get_queryset(self):
         return super().get_queryset().filter(user=self.request.user)
+
+    def get_serializer_class(self):
+        if self.request.method == "POST":
+            serializer_class = OrderCreateSerializer
+        else:
+            serializer_class = self.serializer_class
+        return serializer_class
