5ce5a74e74c736e1ee809754f7bcc15b0a3016d3
Patryk <patryk17x@gmail.com>
2023-12-07 22:24:06 +0100

    feat: ContactUs Page

--- frontend/src/components/ContactUsPage/ContactUsPage.js
+++ frontend/src/components/ContactUsPage/ContactUsPage.js
@@ -1,21 +1,46 @@
-import React from "react";
+import React from 'react'
 
 export const ContactUsPage = () => {
-    return (
-        <div>
-            <ul>
-                <li>ContactUs</li>
-                <li>ContactUs</li>
-                <li>ContactUs</li>
-                <li>ContactUs</li>
-                <li>ContactUs</li>
-                <li>ContactUs</li>
-                <li>ContactUs</li>
-                <li>ContactUs</li>
-                <li>ContactUs</li>
-                <li>ContactUs</li>
-                <li>ContactUs</li>
-            </ul>
+  return (
+        <div className='bg-brown h-screen flex items-center justify-center'>
+           <div className='bg-white w-[1200px] py-8  mx-auto- flex justify-center drop-shadow-2xl rounded-md'>
+            <table className="rounded-md bg-white text-black drop-shadow-lg overflow-hidden">
+              <tbody>
+                <tr className=''>
+                  <th className='py-3 px-4 text-center bg-amber-100 border w-auto'>
+                    <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none">
+                        <path d="M12.398 19.804L12.8585 20.6917L12.8585 20.6917L12.398 19.804ZM11.602 19.804L11.1415 20.6917L11.1415 20.6917L11.602 19.804ZM18 11C18 13.1458 16.9079 14.9159 15.545 16.2906C14.183 17.6644 12.6342 18.555 11.9376 18.9163L12.8585 20.6917C13.6448 20.2838 15.397 19.2805 16.9653 17.6987C18.5326 16.1178 20 13.8706 20 11H18ZM12 5C15.3137 5 18 7.68629 18 11H20C20 6.58172 16.4183 3 12 3V5ZM6 11C6 7.68629 8.68629 5 12 5V3C7.58172 3 4 6.58172 4 11H6ZM12.0624 18.9163C11.3658 18.555 9.81702 17.6644 8.45503 16.2906C7.09211 14.9159 6 13.1458 6 11H4C4 13.8706 5.46741 16.1178 7.03474 17.6987C8.60299 19.2805 10.3552 20.2838 11.1415 20.6917L12.0624 18.9163ZM11.9376 18.9163C11.9514 18.9091 11.9733 18.9023 12 18.9023C12.0267 18.9023 12.0486 18.9091 12.0624 18.9163L11.1415 20.6917C11.6831 20.9726 12.3169 20.9726 12.8585 20.6917L11.9376 18.9163ZM14 11C14 12.1046 13.1046 13 12 13V15C14.2091 15 16 13.2091 16 11H14ZM12 9C13.1046 9 14 9.89543 14 11H16C16 8.79086 14.2091 7 12 7V9ZM10 11C10 9.89543 10.8954 9 12 9V7C9.79086 7 8 8.79086 8 11H10ZM12 13C10.8954 13 10 12.1046 10 11H8C8 13.2091 9.79086 15 12 15V13Z" fill="#33363F"/>
+                    </svg>
+                  </th>
+                  <th className='py-3 px-4 text-center'>
+                    <span className='text-black text-md'>Coffee 27th Street, Suite 527, New York</span>
+                  </th>
+                </tr>
+                <tr className=''>
+                  <th className='py-3 px-4 text-center bg-amber-100 border w-auto'>
+                    <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none">
+                        <path d="M21 12C21 10.1195 20.411 8.28625 19.3156 6.7577C18.2203 5.22915 16.6736 4.08209 14.893 3.47763C13.1123 2.87316 11.187 2.84166 9.38744 3.38754C7.58792 3.93341 6.00459 5.02925 4.85982 6.52115C3.71505 8.01304 3.06635 9.82604 3.00482 11.7055C2.94329 13.585 3.47203 15.4366 4.51677 17.0001C5.56152 18.5637 7.06979 19.7608 8.82975 20.4232C10.5897 21.0856 12.513 21.18 14.3294 20.6933" stroke="#33363F" strokeWidth="2" strokeLinecap="round"/>
+                        <circle cx="12" cy="12" r="4" stroke="#33363F" strokeWidth="2"/>
+                        <path d="M16 9V13.5C16 14.8807 17.1193 16 18.5 16V16C19.8807 16 21 14.8807 21 13.5V12" stroke="#33363F" strokeWidth="2" strokeLinecap="round"/>
+                    </svg>
+                  </th>
+                  <th className='py-3 px-4 text-center'>
+                    <span className='text-black text-md'>illuminati@illuminati.org</span>
+                  </th>
+                </tr>
+                <tr className=''>
+                  <th className='py-3 px-4 text-center bg-amber-100 border w-auto'>
+                    <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none">
+                        <path d="M17.7071 13.7071L20.3552 16.3552C20.7113 16.7113 20.7113 17.2887 20.3552 17.6448C18.43 19.57 15.3821 19.7866 13.204 18.153L11.6286 16.9714C9.88504 15.6638 8.33622 14.115 7.02857 12.3714L5.84701 10.796C4.21341 8.61788 4.43001 5.56999 6.35523 3.64477C6.71133 3.28867 7.28867 3.28867 7.64477 3.64477L10.2929 6.29289C10.6834 6.68342 10.6834 7.31658 10.2929 7.70711L9.27175 8.72825C9.10946 8.89054 9.06923 9.13846 9.17187 9.34373C10.3585 11.7171 12.2829 13.6415 14.6563 14.8281C14.8615 14.9308 15.1095 14.8905 15.2717 14.7283L16.2929 13.7071C16.6834 13.3166 17.3166 13.3166 17.7071 13.7071Z" stroke="#33363F" strokeWidth="2"/>
+                    </svg>
+                  </th>
+                  <th className='py-3 px-4 text-center'>
+                    <span className='text-black text-md'>(21) 372 115 997</span>
+                  </th>
+                </tr>
+              </tbody>
+            </table>
+          </div>
         </div>
-    )
+  )
 }
